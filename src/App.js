276070794c4eb152acad19618839fbbcc937a9b9
ArthurRodrigomarques <ArthurRodrigomarques@users.noreply.github.com>
2023-01-15 22:40:21 -0300

    atualizado

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import NavBar from './components/NavBar';
 import SearchBar from './components/SearchBar';
 import Pokedex from './components/Pokedex';
 import { getPokemon, getPokemonData } from './api';
-
-
+import { FavoriteProvider } from './contexts/FavoriteContext';
 
 
 function App() {
 
-
+ const [page, setPage] = useState(0)
+ const [totalPages, setTotalPages] = useState(0)
  const [loading, setLoading] = useState(false)
  const [pokemons, setPokemons] = useState([])
+ const [favorites, setFavorites] = useState([])
 
+const itensPerPage = 25
 const fetchPokemons = async() => {
   try {
        setLoading(true)
-       const data = await getPokemon()
+       const data = await getPokemon(itensPerPage, itensPerPage * page)
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url)
       })
@@ -25,24 +27,48 @@ const fetchPokemons = async() => {
       const results =await Promise.all(promises)
        setPokemons(results);
        setLoading(false)
+       setTotalPages(Math.ceil(data.count / itensPerPage))
   } catch (error) {
     console.log("fetchPokemons error", error)
   }
  
 }
 useEffect(() => {
-  console.log("carregou")
   fetchPokemons()
-}, [])
+}, [page])
 
-
+const updateFavoritePokemons = (name) => {
+  const updateFavorites = [...favorites]
+  const favoriteIndex = favorites.indexOf(name)
+  if(favoriteIndex >= 0) {
+    updateFavorites.slice(favoriteIndex,1)
+  } else {
+    updateFavorites.push(name)
+  }
+  setFavorites(updateFavorites)
+}
 
   return (
+    <FavoriteProvider
+    value={{
+      favoritePokemons: favorites,
+      updateFavoritePokemons: updateFavoritePokemons,
+    }}
+  >
+
     <div>
       <NavBar />
       <SearchBar />
-      <Pokedex pokemons={pokemons.results} loading={loading}/>
-    </div>
+      <Pokedex 
+        pokemons={pokemons} 
+        loading={loading} 
+        page={page} 
+        totalPages={totalPages} 
+        setPage={setPage} 
+      /> 
+      </div>
+     </FavoriteProvider>
+   
   );
 }
 
@@ -51,6 +77,3 @@ export default App;
 
 
 
-
-// 1:31:00     Pokedex com API, React hooks, useState, useContext, localStorage
-// pasquadev
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useContext } from 'react';
+import FavoriteContext from '../contexts/FavoriteContext';
 
 const NavBar = () => {
+  const {favoritePokemons} = useContext(FavoriteContext)
   const logoImg =
   "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
   return (
@@ -8,6 +11,7 @@ const NavBar = () => {
     <div>
       <img alt="pokeapi-logo" src={logoImg} className="navbar-img" />
     </div>
+    <div>{favoritePokemons.length}❤️</div>
   </nav>
   )
 }
